9b8689bf6465c78deb77926f32aaa5f82ac3fcb7
Kemal Özyön <kemalozyon51@gmail.com>
2024-11-11 07:49:34 +0300

    searchForStrings and Comparision

---
+++ Algorithms/searchForString.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int stringCount(char* string);
+
+int main(void){
+    char* strings[6] = {"battleship", "boot", "cannon", "iron", "thimble","top hat"};
+    char target[100];
+    printf("What is the word you want to find in the set: ");
+    scanf("%s", target);
+    int value = 1; // it is for the comlication we might encounter when we work with the strings that has same chars.
+
+    for (int j = 0; j < 6; j++){
+        if(stringCount(target) == stringCount(strings[j])){
+            for (int i = 0, n = stringCount(target); i < n; i++){ // comparing strings
+                if (target[i] != strings[j][i]){
+                   value = 0;
+                }
+                if(value){
+                    printf("The word you have been searching for is in the %dth index: ", j);
+                    return 0;
+                }
+            }  
+            value = 1;
+        }         
+    }
+    printf("The value you have searched for is not exist in the set.");
+    return 1;
+}
+
+int stringCount(char* string){
+    
+    int i = 0, count = 0;
+    while(string[i] != '\0'){
+        count++;
+        i++;
+    }
+    return count;
+}
